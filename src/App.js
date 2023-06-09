1d32f9aebde0b69ad48e9a18c874869d284441e9
minhvuqg <minhvuqug@gmail.com>
2023-06-09 10:19:58 +0700

    Webkit integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const submitForm = () => {
+    var message = {
+      authorize: document.getElementById("authorize").value,
+    };
+    if (!!window?.webkit?.messageHandlers?.observer?.postMessage(message)) {
+      window.webkit.messageHandlers.observer.postMessage(message);
+    }
+  };
+  const [vis, setVis] = useState("");
+  if (vis === "ios") {
+    return (
+      <div>
+        <button onClick={() => setVis("")}>back</button>
+
+        <div>
+          <label for="authorize">Authorize:</label>
+
+          <input
+            type="authorize"
+            id="authorize"
+            placeholder="Off"
+            name="authorize"
+          />
+        </div>
+
+        <button onClick={submitForm}>Click Call IOS HeathKit</button>
+      </div>
+    );
+  }
+
+  if (vis === "android") {
+    return (
+      <div>
+        <button onClick={() => setVis("")}>back</button>
+        <div>
+          <label for="authorize">Authorize:</label>
+
+          <input
+            type="authorize"
+            id="authorize"
+            placeholder="Off"
+            name="authorize"
+          />
+        </div>
+
+        <button onClick={submitForm}>Click Call Android HeathKit</button>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => setVis("ios")}>Apple Care</button>;
+      <button onClick={() => setVis("android")}>Google Care</button>;
+    </>
   );
 }
 
